2166b1f6b253b02186f8c2dca2354132cc1d1dc3
probro27 <prabhavkhera@hotmail.com>
2022-03-21 14:33:51 -0400

    Migration changes

---
+++ Migrations/20220321183311_NewRevisions.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ErrataManager.Migrations
+{
+    public partial class NewRevisions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Errors_Books_BookNameId",
+                table: "Errors");
+
+            migrationBuilder.RenameColumn(
+                name: "Descision",
+                table: "Errors",
+                newName: "Decision");
+
+            migrationBuilder.RenameColumn(
+                name: "BookNameId",
+                table: "Errors",
+                newName: "BookId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Errors_BookNameId",
+                table: "Errors",
+                newName: "IX_Errors_BookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Errors_Books_BookId",
+                table: "Errors",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Errors_Books_BookId",
+                table: "Errors");
+
+            migrationBuilder.RenameColumn(
+                name: "Decision",
+                table: "Errors",
+                newName: "Descision");
+
+            migrationBuilder.RenameColumn(
+                name: "BookId",
+                table: "Errors",
+                newName: "BookNameId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Errors_BookId",
+                table: "Errors",
+                newName: "IX_Errors_BookNameId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Errors_Books_BookNameId",
+                table: "Errors",
+                column: "BookNameId",
+                principalTable: "Books",
+                principalColumn: "Id");
+        }
+    }
+}
